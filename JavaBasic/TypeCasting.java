2caff0010f9e1f939b49c662eda5c62da063a3c3
Shivam Gupta <sg2109964@gmail.com>
2025-06-20 15:12:37 +0530

    done

--- JavaBasic/TypeCasting.java
+++ JavaBasic/TypeCasting.java
@@ -1,7 +1,15 @@
 public class TypeCasting {
     public static void main(String[] args) {
+        // Widening Casting (automatic)
         int myInt = 54;
         double myDouble = myInt;
         System.out.println("Integer " + myInt + " into double " + myDouble);
+
+        // Narrowing Casting (manually)
+        double myDoubleValue = 584.99;
+        int intValue = (int) myDoubleValue;
+
+        System.out.println("The Double value " + myDoubleValue + " to Integer " + intValue);
+
     }
 }
