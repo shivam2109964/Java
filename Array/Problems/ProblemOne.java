e512c5ea3203ef66b3ffd8bd3b7656f2f56e1e61
Shivam Gupta <sg2109964@gmail.com>
2025-07-01 17:32:08 +0530

    problem one done

--- Array/Problems/ProblemOne.java
+++ Array/Problems/ProblemOne.java
@@ -3,8 +3,26 @@ package Array.Problems;
 // Given an array arr. The task is to find the largest element in the given array. 
 public class ProblemOne {
     public static void main(String[] args) {
-        int[] array = { 10, 324, 45, 90, 9808 };
+        int[] array = { 10, 324, 45, 90, 8 };
+        // Iterative Approach
         System.out.println("Largest element: " + largest(array));
+        // Recursive Approach
+        System.out.println("Largest element by rec approach: " + largestRec(array));
+    }
+
+    public static int largestRec(int[] array) {
+        return findMax(array, 0);
+    }
+
+    public static int findMax(int[] array, int index) {
+        System.out.println("INDEX: " + index);
+        if (index == array.length - 1) {
+            return array[index];
+        }
+        System.out.println("-------");
+        int recMax = findMax(array, index + 1);
+        System.out.println("Index: " + index);
+        return Math.max(recMax, array[index]);
     }
 
     public static int largest(int[] array) {
