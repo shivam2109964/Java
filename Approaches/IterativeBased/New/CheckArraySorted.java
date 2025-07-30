bcaa608dec0875b3b81fa8c7ba07694ea6b98fdc
Shivam Gupta <sg2109964@gmail.com>
2025-07-30 09:38:55 +0530

    Let's start

--- Approaches/IterativeBased/New/CheckArraySorted.java
+++ Approaches/IterativeBased/New/CheckArraySorted.java
@@ -2,7 +2,7 @@ package Approaches.IterativeBased.New;
 
 public class CheckArraySorted {
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+        int[] array = { 1, 2, 33, 4, 5, 6, 7 };
         boolean result = isSorted(array);
         System.out.println(result);
     }
